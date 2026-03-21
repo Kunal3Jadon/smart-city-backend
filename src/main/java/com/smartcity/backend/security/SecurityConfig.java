4a5dfa8d8d43485c46d7556499e1ead297969e7b
Kunal3Jadon <kjadon565@gmail.com>
2026-03-21 20:35:21 +0530

    updated Frontend URL

--- src/main/java/com/smartcity/backend/security/SecurityConfig.java
+++ src/main/java/com/smartcity/backend/security/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("https://smart-city-frontend-dusky.vercel.app/"));
         configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization","Content-Type"));
         configuration.setAllowCredentials(true);
